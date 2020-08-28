59b86fd5f7f31a2dbeacbc7673ed0034c143c508
张文旭 <zhangwxl@dcits.com>
2020-08-28 14:36:49 +0800

    初始化提交

--- zwx-haha-k8s/zwx-haha-k8s-client/src/main/java/zwx/k8s/client/deployment/DeploymentUtils.java
+++ zwx-haha-k8s/zwx-haha-k8s-client/src/main/java/zwx/k8s/client/deployment/DeploymentUtils.java
@@ -76,28 +76,42 @@ public class DeploymentUtils {
 
     public static void main(String[] args) throws IOException, ApiException, InterruptedException {
         ApiClient client = K8sClientInit.getClient();
+        //TODO: debug模式，开发环境打开，生成环境关闭
 //        client.setDebugging(true);
         DeploymentUtils deploymentUtils = new DeploymentUtils(client);
+
+        //腾讯云，esc部署命名空间
         String namespace = "tke-cluster-inspection";
+        //腾讯云，esc部署deployment名称
         String name = "esc-gateway";
-        V1DeploymentList v1DeploymentList = deploymentUtils.queryAllDeployment();
+
+        //TODO: 查询所有的deployment信息
+//        V1DeploymentList v1DeploymentList = deploymentUtils.queryAllDeployment();
 //        System.out.println(v1DeploymentList);
+
+        //TODO: 查询腾旭云 esc deployment信息
         V1Deployment v1Deployment = deploymentUtils.querDeployment(name, namespace);
         System.out.println("名称: " + v1Deployment.getMetadata().getName());
         System.out.println("实例数量: " + v1Deployment.getSpec().getReplicas());
         System.out.println("正在运行实例数: " + v1Deployment.getStatus().getReadyReplicas());
+
+
+        //TODO: 修改腾旭云 esc deployment 副本数量
         String jsonPatchStr = "[{\"op\":\"replace\",\"path\":\"/spec/replicas\", \"value\": " + 5 + " }]";
         V1Patch body = new V1Patch(jsonPatchStr);
         ApiResponse<V1Scale> response = deploymentUtils.updateDeployment(name, namespace, body);
         System.out.println(response.getStatusCode() + "" + response.getHeaders());
 
+        // TODO: 查询腾旭云 esc deployment 当前部署pod数量
         v1Deployment = deploymentUtils.querDeployment(name, namespace);
         System.out.println("名称: " + v1Deployment.getMetadata().getName());
         System.out.println("实例数: " + v1Deployment.getSpec().getReplicas());
         System.out.println("正在运行实例数: " + v1Deployment.getStatus().getReadyReplicas());
 
+        // TODO: 演示3秒，POD动态更新，k8s需要耗时，不是实时响应
         Thread.sleep(3000);
 
+        // TODO: 查询腾旭云 esc deployment 当前部署pod数量
         v1Deployment = deploymentUtils.querDeployment(name, namespace);
         System.out.println("名称: " + v1Deployment.getMetadata().getName());
         System.out.println("实例数: " + v1Deployment.getSpec().getReplicas());
